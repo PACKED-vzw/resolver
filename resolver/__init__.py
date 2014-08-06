db8d4509c3b78ee079c915cf053d687e5f1e601c
Nils Van Geele <nils.vgeele@gmail.com>
2014-08-06 13:19:21 +0200

    Error pages + exception logging

--- resolver/__init__.py
+++ resolver/__init__.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config.from_object('resolver.config.Config')
 
-# TODO: Logging in production only
+# TODO: Logging in production only?
 # TODO: Add log file to config
 
 import logging
@@ -11,5 +11,16 @@ file_handler = logging.FileHandler("application.log")
 file_handler.setLevel(logging.INFO)
 file_handler.setFormatter(logging.Formatter('[%(levelname)s] %(asctime)s -- %(message)s'))
 app.logger.addHandler(file_handler)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('notice.html', title='Page Not Found',
+                           message='The page you requested was not found.')
+
+@app.errorhandler(500)
+def internal_error(e):
+    app.logger.exception(e)
+    return render_template('notice.html', title='Server Error',
+                           message='Something went terribly wrong!')
 
 import resolver.controllers
