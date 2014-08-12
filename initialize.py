91ea69ed88c1a152f6d9a133a421b1a3824be71e
Nils Van Geele <nils.vgeele@gmail.com>
2014-08-12 13:20:39 +0200

    Cleanup

--- initialize.py
+++
@@ -1,6 +0,0 @@
-from resolver.database import db_session, init_db
-from resolver.model import PersistentObject, Document, User
-
-init_db()
-db_session.add(User("admin", "default"))
-db_session.commit()
