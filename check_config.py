b75d15b04d69cf8762a3427791fd5f10f8dbc3bf
Nils Van Geele <nils.vgeele@gmail.com>
2014-08-18 16:55:10 +0200

    Bug fix in check_config.py

--- check_config.py
+++ check_config.py
@@ -5,6 +5,6 @@ if (Config.SALT == "") or (Config.SECRET_KEY == ""):
     from base64 import b64encode
     with open('docker_config.py', 'w') as f:
         f.write("SECRET_KEY = '%s'\n" % b64encode(os.urandom(64)))
-        f.write("SALT = '%s'" % b64encode(os.urandom(64)))
+        f.write("SALT = '%s'\n" % b64encode(os.urandom(64)))
         if os.environ.get('BASE_URL', False):
             f.write("BASE_URL= '%s'\n" % os.environ['BASE_URL'])
