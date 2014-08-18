f7515dde77d67fd4c09633f35178f586141223b3
Nils Van Geele <nils.vgeele@gmail.com>
2014-08-18 15:07:38 +0200

    Updated to Docker configurator

--- check_config.py
+++ check_config.py
@@ -6,3 +6,5 @@ if (Config.SALT == "") or (Config.SECRET_KEY == ""):
     with open('docker_config.py', 'w') as f:
         f.write("SECRET_KEY = '%s'\n" % b64encode(os.urandom(64)))
         f.write("SALT = '%s'" % b64encode(os.urandom(64)))
+        if os.environ.get('BASE_URL', False):
+            f.write("BASE_URL= '%s'\n" % os.environ['BASE_URL'])
