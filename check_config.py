5b015edecaa2846d009f150959539af9ee54b293
Nils Van Geele <nils.vgeele@gmail.com>
2014-09-08 15:37:24 +0200

    And another fix

--- check_config.py
+++ check_config.py
@@ -7,13 +7,13 @@ with open('resolver.cfg', 'w') as f:
     stream = StringIO()
     config = RawConfigParser()
 
-    config.set('', 'BASE_URL', os.environ.get('BASE_URL'))
-    config.set('', 'DATABASE_HOST', 'localhost')
-    config.set('', 'DATABASE_USER', 'root')
-    config.set('', 'DATABASE_PASS', '')
-    config.set('', 'DATABASE_NAME', 'resolver')
-    config.set('', 'SECRET_KEY', b64encode(os.urandom(64)))
-    config.set('', 'SALT', b64encode(os.urandom(64)))
+    config.set('', 'BASE_URL', '"%s"' % os.environ.get('BASE_URL'))
+    config.set('', 'DATABASE_HOST', '"localhost"')
+    config.set('', 'DATABASE_USER', '"root"')
+    config.set('', 'DATABASE_PASS', '""')
+    config.set('', 'DATABASE_NAME', '"resolver"')
+    config.set('', 'SECRET_KEY', '"%s"' % b64encode(os.urandom(64)))
+    config.set('', 'SALT', '"%s"' % b64encode(os.urandom(64)))
 
     config.write(stream)
 
