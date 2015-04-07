0a9fe914e39bbc07ac28175715e43624264efe7a
Pieter De Praetere <pieter@packed.be>
2015-04-07 14:37:01 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+from os import path
+
+def read(fname):
+    return open(path.join(path.dirname(__file__), fname)).read()
+
+setup(
+    name='resolver',
+    version='0.9.0',
+    packages=['resolver', 'resolver.model', 'resolver.controllers'],
+    url='https://github.com/PACKED-vzw/resolver',
+    license='GPL v3',
+    author='Packed vzw',
+    author_email='pieter@packed.be',
+    description='The Resolver application is a tool for creating, managing, and using persistent URIs.',
+    long_description=read ('README.md'),
+    install_requires=[
+        'BeautifulSoup == 3.2.1',
+        'Flask == 0.10.1',
+        'Flask-SQLAlchemy == 1.0',
+        'Flask-WTF == 0.10.0',
+        'Jinja2 == 2.7.3',
+        'MarkupSafe == 0.23',
+        'MySQL-python == 1.2.5',
+        'SQLAlchemy == 0.9.7',
+        'Unidecode == 0.04.16',
+        'WTForms == 2.0.1',
+        'Werkzeug == 0.9.6',
+        'gunicorn == 19.1.0',
+        'itsdangerous == 0.24',
+        'requests == 2.3.0',
+        'wsgiref == 0.1.2'
+    ]
+)
