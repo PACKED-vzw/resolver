23671186b9c46440ad87d37170afe0541b3bb7a2
Pieter De Praetere <pieter@packed.be>
2015-04-07 16:13:42 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
     author_email='pieter@packed.be',
     description='The Resolver application is a tool for creating, managing, and using persistent URIs.',
     long_description=read ('README.md'),
+    include_package_data=True,
     install_requires=[
         'BeautifulSoup == 3.2.1',
         'Flask == 0.10.1',
@@ -21,7 +22,7 @@ setup(
         'Flask-WTF == 0.10.0',
         'Jinja2 == 2.7.3',
         'MarkupSafe == 0.23',
-        'MySQL-python == 1.2.5',
+#        'MySQL-python == 1.2.5',
         'SQLAlchemy == 0.9.7',
         'Unidecode == 0.04.16',
         'WTForms == 2.0.1',
